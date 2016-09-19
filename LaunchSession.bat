5094a1f59b3bc4885f8edc448a2e6296cd8cca20
ONECTA <recharge.ashu@gmail.com>
2016-09-19 10:59:19 +0530

    Launch Chrome for Cookies feature
    Added handle for response code 302 by refreshing cookies

---
+++ LaunchSession.bat
@@ -0,0 +1,3 @@
+pushd %~dp0
+del ".cookies.db"
+"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" intranet.barclays.com --new-window "http://my.barcapint.com/ATS/cgi-bin/view.pl?job_name=wdy_%25&view_type=status&instance=P00"
